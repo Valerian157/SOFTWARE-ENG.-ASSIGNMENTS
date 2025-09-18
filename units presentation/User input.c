3529ee37269218047646e8fbd99fe8f4ff12a019
Valerian157 <sarangevalerian@gmail.com>
2025-09-18 23:01:45 +0300

    ASSIGNMENT CODES

---
+++ units presentation/User input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+double BANK_BALANCE;
+double ID;
+double HEIGHT;
+
+printf("Enter your ID\n");
+scanf("%lf\n", &ID);
+
+
+printf("Enter your HEIGHT\n");
+scanf("%lf\n", &HEIGHT);
+
+printf("Enter your BANK BALANCE\n");
+scanf("%lf\n", &BANK_BALANCE);
+
+
+
+return 0;
+}
---
+++ user input/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+int ID;
+int HEIGHT;
+int BANK_BALANCE;
+
+printf("Enter your ID\n");
+scanf("%d", &ID);
+
+printf("Enter your HEIGHT\n");
+scanf("%d", &HEIGHT);
+
+printf("Enter your BANK BALANCE\n");
+scanf("%d", &BANK_BALANCE);
+
+
+    printf("\nYou entered the following dredentials:\n");
+    printf("\nID : %d\n", ID);
+    printf("HEIGHT : %d \n", HEIGHT);
+    printf("BANK BALANCE : %d\n", BANK_BALANCE);
+
+
+return 0;
+}
+
