1650c97de1b61e52c2fd47ec6857f073c94207ce
Valerian157 <sarangevalerian@gmail.com>
2025-09-19 13:27:30 +0300

    Add files via upload
    Code that outputs units to be done this semester

---
+++ display of semester units.c
@@ -0,0 +1,22 @@
+/*
+NAME:VALERIAN SARANGE
+REG:PA106/25/28839/25
+DESCRIPTION:CODE TO DISPLAY THE UNITS FOR 1ST YEAR SEMESTER 1
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+  printf("THE FOLLOWING ARE THE UNITS FOR THIS SEMESTER:\n");
+  printf("\n1.Introduction to Programming and Problem Solving\n");
+  printf("2.Communication Skills\n");
+  printf("3.Fundamentals of ICT\n");
+  printf("4.Mathematics for Science\n");
+  printf("5.Discrete Mathematics\n");
+  printf("6.Computer Systems and Organization\n");
+  printf("7.System Analysis and Design\n");
+
+    return 0;
+}
