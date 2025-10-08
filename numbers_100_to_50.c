1a0f6870b11bde2ef2193f0794bdcf7898b35501
Valerian157 <sarangevalerian@gmail.com>
2025-10-08 22:38:27 +0300

    NUMBER COUNTER 100 TO 50

---
+++ numbers_100_to_50.c
@@ -0,0 +1,23 @@
+/*
+NAME:VALERIAN SARANGE OGARI
+REG:PA106/G/28839/25
+DESCRIPTION:NUMBER COUNTER 100 TO 50
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+ int i;
+
+ for (i=100; i>49; i--){
+    printf("%d\n", i);
+ }
+
+
+
+    return 0;
+}
